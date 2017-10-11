ad9032c6a8f9c4fdd6bc60b0c240436eabf1964b
img2tab <31696646+img2tab@users.noreply.github.com>
2017-10-11 18:28:10 +0300

    Update zippyshare.sh
    variable name cleanup

--- zippyshare.sh
+++ zippyshare.sh
@@ -2,83 +2,81 @@
 # @Description: zippyshare.com file download script
 # @Author: Live2x
 # @URL: https://github.com/img2tab/zippyshare
-# @Version: 1.0.201710111033
+# @Version: 1.0.201710111127
 # @Date: 2017/10/11
 # @Usage: sh zippyshare.sh url
 
-if [ -z "$1" ]; then
-    echo usage: $0 url
-    echo batch usage: $0 url-list.txt
-    echo file "url-list.txt" must have one url per line
+if [ -z "${1}" ]; then
+    echo "usage: ${0} url"
+    echo "batch usage: ${0} url-list.txt"
+    echo "url-list.txt is a file that contains one zippyshare.com url per line"
     exit
 fi
 
 function zippydownload()
 {
-    if [ -f "cookie.txt" ]; then
-      rm -f cookie.txt
+    prefix="$( echo -n "${url}" | cut -c "11,12,31-38" | sed -e 's/[^A-z0-9]//g' )"
+    cookiefile="${prefix}-cookie.txt"
+    infofile="${prefix}-info.txt"
+
+    if [ -f "${cookiefile}" ]; then
+      rm -f "${cookiefile}"
     fi
 
-    wget -O info.txt "${url}" \
+    wget -O "${infofile}" "${url}" \
     --cookies=on \
     --keep-session-cookies \
-    --save-cookies=cookie.txt \
+    --save-cookies="${cookiefile}" \
     --quiet
 
-# Get cookie
-    if [ -f "cookie.txt" ]; then 
-        jsessionid=$(cat cookie.txt | grep "JSESSIONID" | cut -f7)
-        #echo "JSESSIONID => "$jsessionid
+    # Get cookie
+    if [ -f "${cookiefile}" ]; then 
+        jsessionid="$( cat "${cookiefile}" | grep "JSESSIONID" | cut -f7)"
     else
         echo "can't find cookie file"
         exit
     fi
 
-    if [ -f "info.txt" ]; then
+    if [ -f "${infofile}" ]; then
         # Get url algorithm
-        algorithm=$(cat info.txt | grep -E "dlbutton(.*)\/d\/(.*)" | head -n 1 | cut -d'/' -f4 | cut -d'(' -f2 | cut -d')' -f1)
-        #echo "algorithm => "$algorithm
+        algorithm="$( cat "${infofile}" | grep -E "dlbutton(.*)\/d\/(.*)" | head -n 1 | cut -d'/' -f4 | cut -d'(' -f2 | cut -d')' -f1 )"
 
-        a=$(echo $(( ${algorithm} )) )
+        a="$( echo $(( ${algorithm} )) )"
 
-        # Get server, filename, id, reffer
-        filename=$(cat info.txt | grep "/d/" | cut -d'/' -f5 | cut -d'"' -f1 | grep -o "[^ ]\+\(\+[^ ]\+\)*")
-        #echo "filename => "$filename
+        # Get server, filename, id, ref
+        filename="$( cat "${infofile}" | grep "/d/" | cut -d'/' -f5 | cut -d'"' -f1 | grep -o "[^ ]\+\(\+[^ ]\+\)*" )"
         
-        reffer=$(cat info.txt | grep "property=\"og:url\"" | cut -d'"' -f4 | grep -o "[^ ]\+\(\+[^ ]\+\)*")
-        #echo "reffer => "$reffer
+        ref="$( cat "${infofile}" | grep 'property="og:url"' | cut -d'"' -f4 | grep -o "[^ ]\+\(\+[^ ]\+\)*" )"
 
-        server=`echo "$reffer" | cut -d'/' -f3`
-        #echo "server => "$server
+        server="$( echo "${ref}" | cut -d'/' -f3 )"
 
-        id=`echo "$reffer" | cut -d'/' -f5`
-        #echo "id => "$id
+        id="$( echo "${ref}" | cut -d'/' -f5 )"
     else
         echo "can't find info file"
         exit
     fi
 
-# Build download url
-    dl="http://"$server"/d/"$id"/"$a"/"$filename
-#echo "url => "$dl
+    # Build download url
+    dl="http://${server}/d/${id}/${a}/${filename}"
 
-# Set brower agent
+    # Set brower agent
     agent="Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.125 Safari/537.36"
 
-# Start download file
-    wget -c -O $filename $dl \
+    # Start download file
+    wget -c -O "${filename}" "${dl}" \
     -q --show-progress \
-    --referer='$reffer' \
-    --cookies=off --header "Cookie: JSESSIONID=$jsessionid" \
-    --user-agent='$agent'
+    --referer="${ref}" \
+    --cookies=off --header "Cookie: JSESSIONID=${jsessionid}" \
+    --user-agent="${agent}"
 
-    rm -f cookie.txt
-    rm -f info.txt
+    rm -f "${cookiefile}"
+    rm -f "${infofile}"
 }
 
-if [ -f "$1" ]
+if [ -f "${1}" ]
 then
-    for url in $(cat "${1}" | grep 'zippyshare.com' ); do zippydownload "${url}"; done
+    for url in $( cat "${1}" | grep 'zippyshare.com' ); do zippydownload "${url}"; done
 else
-    zippydownload "${1}"
+    url="${1}"
+    zippydownload "${url}"
 fi
